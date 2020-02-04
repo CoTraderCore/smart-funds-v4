c805c1c66fff1f005ed365642c3dbcda664a3c33
Ruslan <ruslan777mirov@gmail.com>
2020-02-04 12:19:27 +0200

    Refactoring

--- core/interfaces/SmartFundOverrideInterface.sol
+++ core/interfaces/SmartFundOverrideInterface.sol
@@ -1,4 +1,5 @@
-// DAI and ETH have different implements of this method but we require implement this method  
+// DAI and ETH have different implements of this methods but we require implement this method
 contract SmartFundOverrideInterface {
   function calculateFundValue() public view returns (uint256);
+  function getTokenValue(ERC20 _token) public view returns (uint256);
 }
