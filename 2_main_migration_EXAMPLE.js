6ada0d77656b8e6691935d660bb6fc688bac2911
Ruslan <ruslan777mirov@gmail.com>
2020-02-04 18:19:44 +0200

    Update migration file

--- 2_main_migration_EXAMPLE.js
+++ 2_main_migration_EXAMPLE.js
@@ -3,13 +3,17 @@ const ParaswapParams = artifacts.require('./paraswap/ParaswapParams.sol')
 const GetBancorAddressFromRegistry = artifacts.require('./bancor/GetBancorAddressFromRegistry.sol')
 const GetRatioForBancorAssets = artifacts.require('./bancor/GetRatioForBancorAssets.sol')
 
+const ExchangePortal = artifacts.require('./core/portals/ExchangePortal.sol')
+const PoolPortal = artifacts.require('./core/portals/PoolPortal.sol')
+
+const PermittedExchanges = artifacts.require('./core/verification/PermittedExchanges.sol')
+const PermittedStabels = artifacts.require('./core/verification/PermittedStabels.sol')
+const PermittedPools = artifacts.require('./core/verification/PermittedPools.sol')
+
+const SmartFundETHFactory = artifacts.require('./core/SmartFundETHFactory.sol')
+const SmartFundUSDFactory = artifacts.require('./core/SmartFundUSDFactory.sol')
 
 const SmartFundRegistry = artifacts.require('./core/SmartFundRegistry.sol')
-const ExchangePortal = artifacts.require('./core/ExchangePortal.sol')
-const PermittedExchanges = artifacts.require('./core/PermittedExchanges.sol')
-const PermittedPools = artifacts.require('./core/PermittedPools.sol')
-const PoolPortal = artifacts.require('./core/PoolPortal.sol')
-
 
 const PARASWAP_NETWORK_ADDRESS = ""
 const PARASWAP_PRICE_ADDRESS = ""
@@ -17,7 +21,6 @@ const BANCOR_REGISTRY = ""
 const BANCOR_ETH_WRAPPER = ""
 const PRICE_FEED_ADDRESS = ""
 const PLATFORM_FEE = 1000
-
 const STABLE_COIN_ADDRESS = ""
 
 
@@ -48,13 +51,22 @@ module.exports = (deployer, network, accounts) => {
     ))
     .then(() => deployer.deploy(PermittedExchanges, ExchangePortal.address))
 
+    .then(() => deployer.deploy(SmartFundETHFactory))
+
+    .then(() => deployer.deploy(SmartFundETHFactory))
+
+    .then(() => deployer.deploy(PermittedStabels, STABLE_COIN_ADDRESS))
+
     .then(() => deployer.deploy(
       SmartFundRegistry,
       PLATFORM_FEE,
-      ExchangePortal.address,
       PermittedExchanges.address,
+      ExchangePortal.address,
       PermittedPools.address,
       PoolPortal.address,
-      STABLE_COIN_ADDRESS
+      PermittedStabels.address,
+      STABLE_COIN_ADDRESS,
+      SmartFundETHFactory.address,
+      SmartFundUSDFactory.address
     ))
 }
